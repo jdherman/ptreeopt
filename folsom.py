1be684287a7e49c034399facbf27e5ade7018556
jdherman <jdherman8@gmail.com>
2016-06-17 16:27:08 -0700

    more updates to the case study. flood cost added to objective. optimization basically works; fitting historical doesn't. Need to prune trees.

--- folsom.py
+++ folsom.py
@@ -1,12 +1,8 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 from opt import *
-
-# simplified reservoir simulation
-
-df = pd.read_csv('folsom-daily.csv', index_col=0, parse_dates=True)
-df = df['1995-10-01':'2015-09-30']
 
 def water_day(d):
   return d - 274 if d >= 274 else d + 91
@@ -17,79 +13,71 @@ def cfs_to_taf(Q):
 def max_release(S):
   # rule from http://www.usbr.gov/mp/cvp//cvp-cas/docs/Draft_Findings/130814_tech_memo_flood_control_purpose_hydrology_methods_results.pdf
   storage = [0, 100, 400, 600, 1000]
-  release = cfs_to_taf(np.array([0, 35000, 40000, 115000, 130000]))
+  release = cfs_to_taf(np.array([0, 35000, 40000, 115000, 115000])) # make the last one 130 for future runs
   return np.interp(S, storage, release)
+
+
+# simplified reservoir simulation
+df = pd.read_csv('folsom-daily.csv', index_col=0, parse_dates=True)
+df = df['1955-10-01':'2015-09-30']
+Q = df.inflow.values
+K = 975 # capacity, TAF
+dowy = np.array([water_day(d) for d in df.index.dayofyear])
+D = 5 + 2*np.sin(2*np.pi*dowy/365 - np.pi)
+T = len(Q)
 
 
 def f(P, mode='optimization'):
 
-  # Set some parameters
-  K = 975 # capacity, TAF
-
-  # data setup
-  Q = df.inflow.values
-  true_release = df.outflow.values
-  dowy = np.array([water_day(d) for d in df.index.dayofyear])
-  T = len(Q)
-
-  S,R,D = [np.zeros(T) for _ in range(3)]
-
+  S,R = np.zeros(T),np.zeros(T)
   cost = 0
-  ssq = 0
-
   S[0] = df.storage.values[0]
 
   for t in range(1,T):
 
-    # demand: hypothetical model
-    # minimum of 3 TAF/day around January 9th
-    # maximum of 7 TAF/day around mid-July
-    D[t] = 5 + 2*np.sin(2*np.pi*dowy[t]/365 - np.pi)
-
-    # the real-valued way:
+    # if real-valued policy:
     # R[t] = min(P.evaluate([S[t-1], Q[t], dowy[t]]), W)
-
-    TDI = Q[t:t+3].sum()
+    TDI = np.sum(Q[t+1:t+4])
 
     # the discrete way:
-    policy = P.evaluate([S[t-1], TDI])
+    policy = P.evaluate([S[t-1], dowy[t], TDI])
 
     if policy == 'Release_Demand':
       target = D[t]
-    elif policy == 'Hedge':
+    elif policy == 'Hedge_80':
       target = 0.8*D[t]
+    elif policy == 'Hedge_50':
+      target = 0.5*D[t]
     elif policy == 'Flood_Control':
-      target = min(TDI, max_release(S[t-1]))
+      # for optimization, use this. For fitting historical it won't work.
+      target = max(S[t-1]+TDI-K,0)
 
-    # ramping rates
-    p = 0.2
-    R[t] = np.clip(R[t], (1-p)*R[t-1], (1+p)*R[t-1])
+    # a problem: flood control policy contributes to the demand objective
+    # ie you can "satisfy demand" by releasing flood water.
 
-    R[t] = min(target, S[t-1] + Q[t]) # can't release more than available
-    spill = max(S[t-1] + Q[t] - R[t] - K, 0)
-    R[t] +=  spill
+    # ramping rates. need to clean up the logic of this part.
+    # p = 0.2
+    # R[t] = np.clip(R[t], (1-p)*R[t-1], (1+p)*R[t-1])
+
+    # max/min release
+    R[t] = np.clip(R[t], S[t-1] + Q[t], max_release(S[t-1]))
+    R[t] +=  max(S[t-1] + Q[t] - R[t] - K, 0) # spill
     S[t] = S[t-1] + Q[t] - R[t]
 
-    # pick one: either minimize deficit, or fit historical
-    cost += max(D[t] - R[t], 0)**2
+    # squared deficit. Also penalize any total release over 100 TAF/day  
+    # should be able to vectorize this.  
+    cost += max(D[t] - R[t], 0)**2 + max(R[t]-100, 0)**2
 
-    # need to penalize large floods (115k CFS maximum)
-    # do this as just any overtopping, because it can't be controlled anymore
-    # also penalize low storage volumes throughout.
-    if R[t] > 220: # or spill:
-      cost += 10**10
-    # if S[t] < 200:
-    #   cost += 10**5
 
-    # ssq += (R[t] + spill - true_release[t])**2
-
-  if mode is 'simulation':
+  if mode == 'simulation':
     df['Ss'] = pd.Series(S, index=df.index)
     df['Rs'] = pd.Series(R, index=df.index)
     df['demand'] = pd.Series(D, index=df.index)
     return df
   else:
-    return cost #deficit # minimize. If fitting historical.
+    return cost #deficit # minimize. 
+    # or if fitting historical ... (rmse)
+    # return np.sqrt(np.mean((R - df.outflow.values)**2))
 
 
 def plot_results(df):
@@ -106,10 +94,10 @@ def plot_results(df):
 # action_bounds = [0, 100] # max of 115000 cfs
 
 algorithm = PTreeOpt(f, 
-                    feature_bounds = [[0,1000], [0,500]],
-                    feature_names = ['Storage', 'TDI'],
+                    feature_bounds = [[0,1000], [1,365], [0,300]],
+                    feature_names = ['Storage', 'Day', 'TDI'],
                     discrete_actions = True,
-                    action_names = ['Release_Demand', 'Hedge', 'Flood_Control']
+                    action_names = ['Release_Demand', 'Hedge_80', 'Hedge_50', 'Flood_Control']
                     )
 
 # print algorithm.objectives
