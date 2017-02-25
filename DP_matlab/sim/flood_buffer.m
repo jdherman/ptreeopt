d4f824d661fe1a82820cd67295c80d6e6f76e6f2
jdherman <jdherman8@gmail.com>
2017-02-25 09:04:46 -0800

    remove paper specific things from master branch

--- DP_matlab/sim/flood_buffer.m
+++
@@ -1,6 +0,0 @@
-function y = flood_buffer( d )
-
- tp = [1, 50, 151, 200, 243, 365] ;
- sp = [975, 400, 400, 750, 975, 975] ;
- 
- y = interp_lin_scalar( tp, sp, d ) ;
--- DP_matlab/sim/taf_to_cfs.m
+++
@@ -1,5 +0,0 @@
-function y = taf_to_cfs(x)
-
-y = x .* 1000 ./ 86400 .* 43560 ;
-
-end
