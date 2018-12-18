1d5d3ddc4f6161f56c1ba6104293ec763d5a6ded
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2018-12-18 21:58:47 +0100

    small bug fix, improved documentation of example, and logging in main process

--- examples/mpi_example.py
+++ examples/mpi_example.py
@@ -1,8 +1,11 @@
 import sys
 sys.path.append('..')
 
+import logging
+
 import numpy as np
-from .folsom import Folsom
+
+from folsom import Folsom
 from ptreeopt import PTreeOpt, MPIExecutor
 
 # Example to run optimization and save results
@@ -32,10 +35,12 @@ algorithm = PTreeOpt(model.f,
                      )
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,
+        format='[%(processName)s/%(levelname)s:%(filename)s:%(funcName)s] %(message)s')
         
     # With only 1000 function evaluations this will not be very good
-    # should be run with mpiexec -n 6 python -m mipy4py.futures mpi_example.py
+    # should be run with mpiexec -n 6 python -m mpi4py.futures mpi_example.py
     with MPIExecutor() as executor:
-        snapshots = algorithm.run(1000, 10, executor)
+        snapshots = algorithm.run(1000, 10, executor=executor)
 
 # pickle.dump(snapshots, open('example-results.pkl', 'wb'))
