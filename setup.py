fb8bfe2bb3023dc0c886ea287264b7322cbc9400
John Kucharski <johnkucharski@gmail.com>
2021-09-22 10:56:50 -1000

    add: build (setup.py) action (p*.yml) env (e*.yml)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='ptreeopt',
+      version='1.0.1',
+      author='Jonathan Herman, Matteo Guilliani',
+      author_email='jdherman@ucdavis.edu',
+      url='https://github.com/jdherman/ptreeopt',
+      description='A heuristic policy search model designed for the conrol of dynamic systems.',
+      long_description = 'A heuristic policy search model designed for the control of dynamic systems. The model uses genetic programming to develop binary trees relating observed indicator variables to real-valued or discrete actions. A simulation model serves as optimization models objective function. Citation: Herman, J.D. and Giuliani, M. Policy tree optimization for threshold-based water resources management over multiple timescales, Environmental Modelling and Software, 99, 39-51, 2018.',
+      packages=find_packages(),
+      license = 'MIT',    
+      )
